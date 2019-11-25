35d632f3716615f01a44cad3054ba2e33ccf943c
Kevin <KCHEN21@d-e.org>
2019-11-25 15:45:05 -0500

    Good Vibes

---
+++ ftc_app_new/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Active/RedFoundation.java
@@ -0,0 +1,140 @@
+package org.firstinspires.ftc.teamcode.Autonomous.Active;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.teamcode.Hardware.*;
+import org.firstinspires.ftc.teamcode.Autonomous.*;
+
+@Autonomous(name = "Red Foundation", group = "Autonomous")
+public class RedFoundation extends OpMode {
+    private ElapsedTime runtime = new ElapsedTime();
+    private DigitalChannel DigChannel;
+    AutonMethods robot = new AutonMethods();
+
+    public int auto = 0;
+
+    int center = 150;
+    int left = 600;
+    int right = 350;
+
+    int centerBack = 1100;
+    int leftBack = 800;
+    int rightBack = 1750;
+
+    int curVal = 0;
+
+    public static Servo clamp;
+
+    public void init() {
+        robot.init(hardwareMap, telemetry, false);
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        clamp = this.hardwareMap.get(Servo.class, "clamp");
+
+        robot.BR.setDirection(DcMotorSimple.Direction.FORWARD);
+        robot.BL.setDirection(DcMotorSimple.Direction.REVERSE);
+        robot.FL.setDirection(DcMotorSimple.Direction.REVERSE);
+        robot.FR.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        robot.FL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        robot.BL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        robot.BR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        robot.FR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        robot.changeRunMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
+     */
+    @Override
+    public void init_loop() {
+    }
+
+    /*
+     * Code to run ONCE when the driver hits PLAY
+     */
+    @Override
+    public void start() {
+        runtime.reset();
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
+     */
+    @Override
+    public void loop() {
+        switch (robot.command) {
+            case 0:
+                this.clamp.setPosition(1);
+                robot.openServo();
+                robot.runToTarget(Movement.BACKWARD, 35, false);
+                break;
+
+            case 1:
+                robot.encoderReset();
+                break;
+
+            case 2:
+                robot.runToTarget(Movement.RIGHTSTRAFE, 77,  true);
+                break;
+
+            case 3:
+                robot.closeServoAuton();
+                break;
+
+            case 4:
+                robot.runToTarget(Movement.LEFTSTRAFE, 77,  true);
+                break;
+
+            case 5:
+                robot.encoderReset();
+                break;
+
+            case 6:
+                robot.runToTarget(Movement.LEFTTURN , 100,  false);
+                break;
+
+            case 7:
+                robot.openServoAuton();
+                break;
+
+            case 8:
+                robot.runToTarget(Movement.RIGHTSTRAFE , 40,  true);
+                break;
+
+            case 9:
+                robot.encoderReset();
+                break;
+
+            case 10:
+                robot.runToTarget(Movement.BACKWARD , 50,  false);
+                break;
+
+            case 11:
+                robot.encoderReset();
+                break;
+
+            case 12:
+                robot.runToTarget(Movement.LEFTSTRAFE , 80,  true);
+                break;
+
+            case 13:
+                robot.changeRunMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                break;
+        }
+        telemetry.addData("Case:", robot.command);
+        telemetry.update();
+    }
+}
