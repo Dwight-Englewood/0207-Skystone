0f999fd1ade46215c6e5bb8e950e42e6d2418935
Matt <mccorm@d-e.org>
2020-01-15 16:33:39 -0500

    javadoc

--- ftc_app_new/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/DeuxBoot.java
+++ ftc_app_new/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/DeuxBoot.java
@@ -17,8 +17,15 @@ DeuxBoot {
 
     public RevBlinkinLedDriver blinkin;
 
-    public DeuxBoot() {}
+    public DeuxBoot() {
+    }
 
+    /**
+     *
+     * @param map creates an object on phone
+     * @param tele displays data on phone
+     * @param auton determines if it is auton.
+     */
     public void init(HardwareMap map, Telemetry tele, boolean auton) {
         this.map = map;
         this.tele = tele;
@@ -59,6 +66,12 @@ DeuxBoot {
         tele.update();
     }
 
+    /**
+     *
+     * Changes the motor run mode
+     *
+     * @param runMode the new runmode.
+     */
     public static void changeRunMode(DcMotor.RunMode runMode) {
         BL.setMode(runMode);
         BR.setMode(runMode);
@@ -66,6 +79,15 @@ DeuxBoot {
         FR.setMode(runMode);
     }
 
+    /**
+     *
+     *Sets the motor power levels according to inputs from the controllers Two stick
+     *
+     * @param leftStick_y the y power values from the left stick on the controllers (-1,1)
+     * @param rightStick_y the y power values from the right stick on the controllers (-1,1)
+     * @param leftTrigger the power values from the left trigger
+     * @param rightTrigger the power values from the right trigger
+     */
     public void drive(double leftStick_y, double rightStick_y, double leftTrigger, double rightTrigger) {
         if (leftTrigger > .3) {
             drive(Movement.LEFTSTRAFE, leftTrigger * 0.75);
@@ -81,6 +103,15 @@ DeuxBoot {
         BR.setPower(rightStick_y);
     }
 
+    /**
+     *
+     * Sets the motor power levels according to inputs from the controllers. Uses one stick.
+     *
+     * @param leftStick_y The y power level from the left stick on the controller (-1,1)
+     * @param leftStick_x The x power level from the left stick on the controller(-1,1)
+     * @param leftTrigger The power levels from the left Trigger
+     * @param rightTrigger The power levels from the left Trigger.
+     */
     public void notKevinDrive(double leftStick_y, double leftStick_x, double leftTrigger, double rightTrigger) {
         if (leftTrigger > .3) {
             drive(Movement.LEFTSTRAFE, leftTrigger * 0.75);
@@ -102,7 +133,13 @@ DeuxBoot {
         BR.setPower(leftStick_x);
     }
 
-    //TODO fix the the driver values and restrict the motor values
+    /**
+     *
+     * Sets motor power levels to specified power levels..
+     *
+     * @param movement Direction of movement.
+     * @param power level of power that motor will be set to.
+     */
     public void drive(Movement movement, double power) {
         switch (movement) {
             case FORWARD:
